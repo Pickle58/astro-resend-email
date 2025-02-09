af32aa28dff8f944a02a71e1d1077de2777b62e2
Pickle58 <jpilkington332@gmail.com>
2025-02-09 08:05:26 -0700

    send.ts and .env

---
+++ src/pages/api/send.ts
@@ -0,0 +1,20 @@
+import type { APIRoute } from "astro";
+import { Resend } from "resend";
+
+const resend = new Resend(import.meta.env.RESEND_API_KEY);
+
+export const GET: APIRoute = async () => {
+    //send email
+   const { data, error } = await resend.emails.send({
+        from: "James <james.pilkington@pilkington.info>",
+        to: ["deliverd@resend.dev"],
+        subject: "It works!",
+        html: "<p>Hello world</p>",
+    });
+
+    if (error) {
+        return new Response(JSON.stringify(error));
+      }
+    
+      return new Response(JSON.stringify(data));
+    };
